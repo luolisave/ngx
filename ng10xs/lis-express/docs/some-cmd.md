aa0d9a5257d13db8733396cf37373beff08560bd
luoli <luolisave@gmail.com>
2026-04-21 09:39:40 -0400

    remove old code

--- ng10xs/lis-express/docs/some-cmd.md
+++
@@ -1,3 +0,0 @@
-
-
-ng generate module notes-routing --flat --module=notes
--- ng7xs/src/app/test/actions/user.action.ts
+++
@@ -1,7 +0,0 @@
-import { User } from '../models/User';
-
-export class AddUser {
-    static readonly type = '[User] Add';
-
-    constructor(public payload: User) {}
-}
